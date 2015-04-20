ad4ed0d4e4a27ed99a9584c12383a06bdd0bcb44
Henrik Joreteg <hjoreteg@gmail.com>
2015-04-20 14:37:25 -0700

    Mon Apr 20 14:37:25 PDT 2015

--- src/router.js
+++ src/router.js
@@ -3,8 +3,19 @@ import Router from 'ampersand-router'
 import React from 'react'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
+import Layout from './layout'
 
 export default Router.extend({
+  renderPage (Page, opts) {
+    const Main = (
+      <Layout>
+        <Page/>
+      </Layout>
+    )
+
+    React.render(Main, document.body)
+  },
+
   routes: {
     '': 'public',
     'repos': 'repos'
@@ -15,6 +26,6 @@ export default Router.extend({
   },
 
   repos () {
-    React.render(<ReposPage/>, document.body)
+    this.renderPage(ReposPage)
   }
 })
