abf4c69f99542580a9ba28f4ffabf3a11f67f907
Henrik Joreteg <hjoreteg@gmail.com>
2015-04-20 15:13:52 -0700

    Mon Apr 20 15:13:52 PDT 2015

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import app from './app'
 import Router from 'ampersand-router'
+import qs from 'qs'
 import React from 'react'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
@@ -18,7 +19,9 @@ export default Router.extend({
 
   routes: {
     '': 'public',
-    'repos': 'repos'
+    'repos': 'repos',
+    'login': 'login',
+    'auth/callback?code=:code': 'authCallback'
   },
 
   public () {
@@ -27,5 +30,17 @@ export default Router.extend({
 
   repos () {
     this.renderPage(ReposPage)
+  },
+
+  login () {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      scope: 'user,repo',
+      redirect_uri: window.location.origin + '/auth/callback',
+      client_id: 'f8dd69187841cdd22a26'
+    })
+  },
+
+  authCallback (code) {
+    console.log('code from redirect', code)
   }
 })
